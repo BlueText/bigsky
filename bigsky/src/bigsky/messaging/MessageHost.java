9dae2a889f7498f37831584268f562982c3c52ba
jdmielke <jdmielke@iastate.edu>
2013-10-21 00:04:15 -0500

    merge output

--- bigsky/src/bigsky/messaging/MessageHost.java
+++ bigsky/src/bigsky/messaging/MessageHost.java
@@ -1,17 +1,12 @@
 package bigsky.messaging;
 
 import java.net.*;
-import java.awt.TrayIcon.MessageType;
 import java.io.*;
 
 import bigsky.Contact;
 import bigsky.TextMessage;
 
 class ClientConn implements Runnable {
-	
-	public TextMessage st1;
-	public boolean newMessage = false;
-	
 	Thread t;
 	Socket client = null;
 
@@ -23,22 +18,11 @@ class ClientConn implements Runnable {
 
 	public void run() {
 		ObjectInputStream br = null;
-		newMessage = false;
 		try {
 			br = new ObjectInputStream(client.getInputStream());
 			while (br != null) {
-				st1 = ((TextMessage) br.readObject());
-				System.out.println("Client: " + st1.getContent());
-				newMessage = true;
-				
-				if(st1.getSender() == null){
-					st1.setSender(TaskBar.you);
-					st1.setReceiver(TaskBar.me);
-				}
-				TaskBar.textHistory.add(st1);
-				TaskBar.trayIcon.displayMessage("New Message", "message from:\t" + st1.getSender(), MessageType.INFO);
-				TaskBar.smallChatWindow.recievedText(st1);
-         	   //TaskBar.notification.displayMessage("New Message", "NEW MESSAGE", null);
+				String st1 = ((TextMessage) br.readObject()).getContent();
+				System.out.println("Client: " + st1);
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage() + " inside run()");
@@ -46,48 +30,35 @@ class ClientConn implements Runnable {
 	}
 }
 
-class MessageHost extends Thread{
-	
-	public ClientConn conn = null;
-	public static ObjectOutputStream ps2;
-	
-	public void run(){
-		
-		
+class MessageHost {
+	public static void main(String args[]) throws IOException {
+
 		ServerSocket socket = null;
 		try{
 			socket = new ServerSocket(1300);
 			System.out.print("Waiting for request from peer.....");
 			Socket client = socket.accept();
-			conn = new ClientConn(client);
+			@SuppressWarnings("unused")
+			ClientConn conn = new ClientConn(client);
 			System.out.println("request accepted!\nBeginning of chat:");
 			BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-			ps2 = new ObjectOutputStream(client.getOutputStream());
+			ObjectOutputStream ps2 = new ObjectOutputStream(client.getOutputStream());
 
-			//Contact tempContact = new Contact("Andy", "G",    "+1 5072542815", null);
+			Contact tempContact = new Contact("Andy", "G",    "+1 5072542815", null);
 			//Contact tempContact = new Contact("Travis", "Reed", "+1 5633817739", null);
 
 			while (true) {
 				String servMsg = br2.readLine();
-				String textFromSmallChat = new String(TaskBar.myTextHistory.get(SmallChat.getMyTextCount()).getContent());
-				if(textFromSmallChat == null ||  textFromSmallChat.equalsIgnoreCase("quit")){
+				if(servMsg == null ||  servMsg.equalsIgnoreCase("quit")){
 					return;
 				}
-				TextMessage textMsg = new TextMessage(TaskBar.me, TaskBar.you, textFromSmallChat);
-				ps2.writeObject(textMsg);
+				ps2.writeObject(new TextMessage(null, tempContact, servMsg));
 				ps2.flush();
 			}
-		} catch(Exception e){
-			
-		}
+		} 
 		finally{
 			System.out.println("Closing server socket.");
-			if(socket != null)
-				try {
-					socket.close();
-				} catch (IOException e) {
-					
-				}
+			if(socket != null) socket.close();
 
 		}
 	}
